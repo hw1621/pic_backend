02843adedb4d1fd1ebff0aae404e84d216248b68
hw1621 <wanghaoran030401@163.com>
2025-07-28 00:05:23 +0800

    add common utils and components

--- src/main/java/com/example/picture/backend/demo/DemoApplication.java
+++ src/main/java/com/example/picture/backend/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.picture.backend.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.example.picture.backend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class DemoApplication {
 
 	public static void main(String[] args) {
