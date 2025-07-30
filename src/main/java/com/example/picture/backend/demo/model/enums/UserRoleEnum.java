4b2853acdaf30a685f141a88fe92210d258b2e92
hw1621 <wanghaoran030401@163.com>
2025-07-31 02:04:56 +0800

    and enum

---
+++ src/main/java/com/example/picture/backend/demo/model/enums/UserRoleEnum.java
@@ -0,0 +1,39 @@
+package com.example.picture.backend.demo.model.enums;
+
+import cn.hutool.core.util.ObjUtil;
+import lombok.Getter;
+
+@Getter
+public enum UserRoleEnum {
+
+    USER("用户", "user"),
+    ADMIN("管理员", "admin");
+
+    private final String text;
+
+    private final String value;
+
+    UserRoleEnum(String text, String value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    /**
+     * Get values based on enum types
+     *
+     * @param value values of enums
+     * @return enums
+     */
+    public static UserRoleEnum getEnumByValue(String value) {
+        if (ObjUtil.isEmpty(value)) {
+            return null;
+        }
+        for (UserRoleEnum anEnum : UserRoleEnum.values()) {
+            if (anEnum.value.equals(value)) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+}
+
