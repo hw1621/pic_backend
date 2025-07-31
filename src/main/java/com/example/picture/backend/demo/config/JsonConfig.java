6cca47af2ac9d564b3a3fb8044df843e4151137b
hw1621 <wanghaoran030401@163.com>
2025-08-01 01:39:12 +0800

    add global json long handler to avoid accuracy missing during transaction

---
+++ src/main/java/com/example/picture/backend/demo/config/JsonConfig.java
@@ -0,0 +1,22 @@
+package com.example.picture.backend.demo.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@JsonComponent
+public class JsonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        return  objectMapper;
+    }
+}
