903d7f7eff30f4d81e3d24805db18a024214de13
hw1621 <wanghaoran030401@163.com>
2025-08-06 23:34:59 +0800

    add picture upload and download functionality

---
+++ src/main/java/com/example/picture/backend/demo/controller/FileController.java
@@ -0,0 +1,81 @@
+package com.example.picture.backend.demo.controller;
+
+import com.example.picture.backend.demo.annotation.AuthCheck;
+import com.example.picture.backend.demo.common.BaseResponse;
+import com.example.picture.backend.demo.common.ResultUtils;
+import com.example.picture.backend.demo.constant.UserConstant;
+import com.example.picture.backend.demo.exception.BusinessException;
+import com.example.picture.backend.demo.exception.ErrorCode;
+import com.example.picture.backend.demo.manager.CosManager;
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.COSObjectInputStream;
+import com.qcloud.cos.utils.IOUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+    @Resource
+    private CosManager cosManager;
+
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @PostMapping("/test/upload")
+    public BaseResponse<String> testUploadFile(@RequestPart("file") MultipartFile multipartFile) {
+        String filename = multipartFile.getOriginalFilename();
+        String filepath = String.format("/test/%s", filename);
+        File file = null;
+        try {
+            file = File.createTempFile(filepath, null);
+            multipartFile.transferTo(file);
+            cosManager.putObject(filepath, file);
+            return ResultUtils.success(filepath);
+        } catch (Exception e) {
+            log.error("file upload error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            if (file != null) {
+                // 删除临时文件
+                boolean delete = file.delete();
+                if (!delete) {
+                    log.error("file delete error, filepath = {}", filepath);
+                }
+            }
+        }
+    }
+
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public void testDownloadFile(String filepath, HttpServletResponse response) throws IOException {
+        COSObjectInputStream cosObjectInputStream = null;
+        try {
+            COSObject cosObject = cosManager.getObject(filepath);
+            cosObjectInputStream = cosObject.getObjectContent();
+            byte[] bytes = IOUtils.toByteArray(cosObjectInputStream);
+            response.setContentType("application/octet-stream;charset=utf-8");
+            response.setHeader("Content-Disposition", "attachment;filename=" + filepath);
+
+            response.getOutputStream().write(bytes);
+            response.getOutputStream().flush();
+        } catch (Exception e) {
+            log.error("file download error, filepath = " + filepath, e);
+            throw new RuntimeException(e);
+        } finally {
+            if (cosObjectInputStream != null) {
+                cosObjectInputStream.close();
+            }
+        }
+
+    }
+
+}
