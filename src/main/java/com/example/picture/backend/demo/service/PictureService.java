46a4ebbf1d8682acc2cb4e2de02b2ae8b9995490
hw1621 <wanghaoran030401@163.com>
2025-08-08 16:36:01 +0800

    add picture service that insert uploaded picture into database after picture is uploaded

--- src/main/java/com/example/picture/backend/demo/service/PictureService.java
+++ src/main/java/com/example/picture/backend/demo/service/PictureService.java
@@ -1,7 +1,11 @@
 package com.example.picture.backend.demo.service;
 
+import com.example.picture.backend.demo.model.dto.picture.PictureUploadRequest;
 import com.example.picture.backend.demo.model.entity.Picture;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.picture.backend.demo.model.entity.User;
+import com.example.picture.backend.demo.model.vo.PictureVO;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
 * @author hw1621
@@ -9,5 +13,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2025-08-06 23:36:30
 */
 public interface PictureService extends IService<Picture> {
+
+    PictureVO uploadPicture(MultipartFile multipartFile,
+                            PictureUploadRequest pictureUploadRequest,
+                            User loginUser);
 
 }
--- src/main/java/com/example/picture/backend/demo/service/impl/PictureServiceImpl.java
+++ src/main/java/com/example/picture/backend/demo/service/impl/PictureServiceImpl.java
@@ -1,10 +1,21 @@
 package com.example.picture.backend.demo.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.picture.backend.demo.exception.ErrorCode;
+import com.example.picture.backend.demo.exception.ThrowUtils;
+import com.example.picture.backend.demo.manager.FileManager;
+import com.example.picture.backend.demo.model.dto.file.UploadPictureResult;
+import com.example.picture.backend.demo.model.dto.picture.PictureUploadRequest;
 import com.example.picture.backend.demo.model.entity.Picture;
+import com.example.picture.backend.demo.model.entity.User;
+import com.example.picture.backend.demo.model.vo.PictureVO;
 import com.example.picture.backend.demo.service.PictureService;
 import com.example.picture.backend.demo.mapper.PictureMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 /**
 * @author hw1621
@@ -14,6 +25,47 @@ import org.springframework.stereotype.Service;
 @Service
 public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
     implements PictureService{
+
+    @Resource
+    private final FileManager fileManager;
+
+    public PictureServiceImpl(FileManager fileManager) {
+        this.fileManager = fileManager;
+    }
+
+    @Override
+    public PictureVO uploadPicture(MultipartFile multipartFile, PictureUploadRequest pictureUploadRequest, User loginUser) {
+        ThrowUtils.throwIf(loginUser == null, ErrorCode.NO_AUTH_ERROR);
+        Long pictureId = null;
+        if (pictureUploadRequest != null) {
+            pictureId = pictureUploadRequest.getId();
+        }
+        if (pictureId != null) {
+            boolean exists = this.lambdaQuery()
+                    .eq(Picture::getId, pictureId)
+                    .exists();
+            ThrowUtils.throwIf(!exists, ErrorCode.NOT_FOUND_ERROR, "图片不存在");
+        }
+
+        String uploadPathPrefix = String.format("public/%s", loginUser.getId());
+        UploadPictureResult uploadPictureResult = fileManager.uploadPicture(multipartFile, uploadPathPrefix);
+        Picture picture = new Picture();
+        picture.setUrl(uploadPictureResult.getUrl());
+        picture.setName(uploadPictureResult.getPicName());
+        picture.setPicSize(uploadPictureResult.getPicSize());
+        picture.setPicWidth(uploadPictureResult.getPicWidth());
+        picture.setPicHeight(uploadPictureResult.getPicHeight());
+        picture.setPicScale(uploadPictureResult.getPicScale());
+        picture.setPicFormat(uploadPictureResult.getPicFormat());
+        picture.setUserId(loginUser.getId());
+        if (pictureId != null) {
+            picture.setId(pictureId);
+            picture.setEditTime(new Date());
+        }
+        boolean result = this.saveOrUpdate(picture);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR, "图片上传失败");
+        return PictureVO.objToVo(picture);
+    }
 
 }
 
